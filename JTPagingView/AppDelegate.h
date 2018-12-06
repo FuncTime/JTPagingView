3fa4b3d0fe0f73b7ef6d9f34e549c5b2e6bc0791
xujiangtao <408892334@qq.com>
2018-12-06 14:02:17 +0800

    创建项目

---
+++ JTPagingView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JTPagingView
+//
+//  Created by xujiangtao on 2018/12/6.
+//  Copyright © 2018年 xujiangtao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JTPagingView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JTPagingView
+//
+//  Created by xujiangtao on 2018/12/6.
+//  Copyright © 2018年 xujiangtao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
